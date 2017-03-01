413919bde7f8e802b358e31f514b4cf884ba052d
Blake Impecoven <bimpecoven@Blakes-MacBook-Pro.local>
2017-03-01 01:45:52 -0800

    TESTER NOT BY ME

--- TestMyQuery.java
+++ TestMyQuery.java
@@ -10,15 +10,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestMyQuery {
-        public static void main(String[] args) {                
+        public static void main(String[] args) {
             try {
                     Connection conn = getConnection();
                     MyQuery mquery = new MyQuery(conn);
-                    
+
      					  // Query 0
                     mquery.findChargeDate();
                     mquery.printChargeDate();
-						              
+
 						  // Query 1
                     mquery.findBestOfficer();
                     mquery.printBestOfficer();
@@ -26,26 +26,26 @@ public class TestMyQuery {
                     // Query 2
                     mquery.findCrimeCharge();
                     mquery.printCrimeCharge();
-                  
+
                     // Query 3
                     mquery.findCriminal();
                     mquery.printCriminal();
-                    
+
                     // Query 4
                     mquery.findCriminalSentence();
                     mquery.printCriminalSentence();
-                  
+
                     // Query 5
                     mquery.findChargeCount();
                     mquery.printChargeCount();
-						  
+
 						  // Query 6
                     mquery.findEarliestLatest();
                     mquery.printEarliestLatest();
- 					
+
 						  // Query 7
 						 mquery.findCrimeCounts();
-				
+
                     conn.close();
             } catch (SQLException e) {
                     e.printStackTrace();
@@ -54,9 +54,9 @@ public class TestMyQuery {
                     e.printStackTrace();
             }
         }
-        
+
         public static Connection getConnection() throws SQLException{
-            Connection connection; 
+            Connection connection;
             try {
                     Class.forName("com.mysql.jdbc.Driver").newInstance();
             } catch (InstantiationException e1) {
@@ -70,8 +70,8 @@ public class TestMyQuery {
             String serverName = "146.187.134.44:3306";
             String mydatabase = "f16impecoven_crime";
             String url = "jdbc:mysql://" + serverName + "/" + mydatabase; // a JDBC url
-            String username = "f16impecoven";
-            String password = "5647lois";
+            String username = "username"; //replace with actual data
+            String password = "password"; //replace with actual data
             connection = DriverManager.getConnection(url, username, password);
             return connection;
         }
